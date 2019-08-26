e3861674ecc795930af79a57a1a0af127a304b29
pradeepkumaresan <pradeep.it39@gmail.com>
2019-08-26 22:36:50 +0530

    implementing linked list

---
+++ src/main/java/com/pradeep/linkedList/LinkedList.java
@@ -0,0 +1,94 @@
+package com.pradeep.linkedList;
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    public void addLast(T t) {
+        if (head == null) {
+            head = new Node<>(t);
+            tail = head;
+            return;
+        }
+        Node<T> node = new Node<>(t);
+        tail.setNext(node);
+        node.setPrevious(tail);
+        tail = node;
+    }
+
+    public T removeLast() throws Exception {
+        if (tail == null) {
+            throw new Exception("list is empty");
+        }
+        if(tail.getPrevious() == null) {
+            T val = tail.value;
+            head = tail = null;
+            System.out.println(val + " removed.");
+            return val;
+        }
+        Node<T> returnVal = tail;
+        tail = tail.getPrevious();
+        tail.setNext(null);
+        System.out.println(returnVal.value + " removed.");
+        return returnVal.getValue();
+    }
+
+    public void traverse(){
+        System.out.print("Traversing List - ");
+        Node<T> node = head;
+        while(node != null){
+            System.out.print(node.value + " ");
+            node = node.nextElement;
+        }
+        System.out.println();
+    }
+
+    private class Node<T> {
+        private T value;
+        private Node<T> nextElement;
+        private Node<T> previousElement;
+
+        Node(T t) {
+            value = t;
+        }
+
+        T getValue() {
+            return value;
+        }
+
+        void setValue(T t) {
+            this.value = t;
+        }
+
+        Node<T> getPrevious() {
+            return previousElement;
+        }
+
+        void setPrevious(Node<T> previousElement) {
+            this.previousElement = previousElement;
+        }
+
+        void setNext(Node<T> node) {
+            nextElement = node;
+        }
+
+        Node<T> getNextElement() {
+            return nextElement;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/pradeep/linkedList/LinkedListTest.java
@@ -0,0 +1,28 @@
+package com.pradeep.linkedList;
+
+public class LinkedListTest {
+    public static void main(String[] args) throws Exception{
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addLast(1);
+        list.addLast(2);
+        list.traverse();
+
+        list.addLast(3);
+        list.addLast(4);
+        list.traverse();
+
+        list.addLast(5);
+        list.traverse();
+
+        list.removeLast();
+        list.removeLast();
+        list.removeLast();
+        list.traverse();
+
+        list.removeLast();
+        list.traverse();
+
+        list.removeLast();
+        list.traverse();
+    }
+}
